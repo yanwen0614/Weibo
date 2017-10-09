ba06ef556ac09bc8b8320625dabf9d0228f215c5
StarCyan <yanwen0614@gmail.com>
2017-10-09 11:41:55 +0800

    fix bug

--- sina_spider/utils/user.py
+++ sina_spider/utils/user.py
@@ -1,23 +1,25 @@
 from os import sep as ossep
-_user_set = set()
 
-def __init__(self):
-    f = open(ossep.join('.','sina_spider','utils','userlist'), encoding='UTF-8')
-    for line in f.readlines():
-        self.add_user(*line[:-1].rsplit(' '))  
-        '''add your account information at userlist file 
-        like this :
-        username1 password1
-        username2 password2
-        '''
-    
+class UserSet(object):
+    _user_set = set()
 
-@property
-def user_set(self):
-    return sorted(self._user_set)
+    def __init__(self):
+        f = open(ossep.join('.','sina_spider','utils','userlist'), encoding='UTF-8')
+        for line in f.readlines():
+            self.add_user(*line[:-1].rsplit(' '))  
+            '''add your account information at userlist file 
+            like this :
+            username1 password1
+            username2 password2
+            '''
+        
 
-def add_user(self, user, password):
-    self._user_set.add((str(user), str(password)))
+    @property
+    def user_set(self):
+        return sorted(self._user_set)
+
+    def add_user(self, user, password):
+        self._user_set.add((str(user), str(password)))
 
 
 
